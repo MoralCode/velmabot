3c42f087b7af80c392c8198c029c7fea2e4b82f7
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-06-19 20:06:41 -0700

    add method to grab data from the velma dashboard

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import discord
 import os
+import asyncio
 import schedule
+import aiohttp
 
 client = discord.Client()
 
@@ -21,11 +23,21 @@ async def on_message(message):
 	if message.content.startswith('$hello'):
 		await message.channel.send('Hello!')
 
+# https://discordpy.readthedocs.io/en/latest/faq.html#what-does-blocking-mean
+async def send_current_velma_count(channel):
+	# channel = discord.utils.get(guild.text_channels, name="Name of channel")
+	async with aiohttp.ClientSession() as session:
+		async with session.get('https://vial.calltheshots.us/dashboard/public-velma-remaining/') as r:
+			if r.status == 200:
+				text = await r.text()
+				print(text)
+
+				# await channel.send(js['file'])
 
 async def job():
 	print("I'm working...")
 	
-	# send_current_velma_count(client.get_channel("532448188901228570")) # bot-spam area51
+	send_current_velma_count(client.get_channel("532448188901228570")) # bot-spam area51
 
 
 schedule.every(10).seconds.do(job)
